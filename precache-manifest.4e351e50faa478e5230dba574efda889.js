9a7e7178c1419826563430f52db489f66a8d9db5
lukaszlop <66628215+lukaszlop@users.noreply.github.com>
2020-08-27 23:06:00 +0200

    Build files
    Build files

---
+++ precache-manifest.4e351e50faa478e5230dba574efda889.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7912260cd87b2291fa879c0256e404ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d7bc55e92d49fcc0de3",
+    "url": "/static/css/2.9a9b1a2c.chunk.css"
+  },
+  {
+    "revision": "9c31c96e5cb9b3664e04",
+    "url": "/static/css/main.c5eb8982.chunk.css"
+  },
+  {
+    "revision": "8d7bc55e92d49fcc0de3",
+    "url": "/static/js/2.d6e90136.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/static/js/2.d6e90136.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c31c96e5cb9b3664e04",
+    "url": "/static/js/main.f12f2331.chunk.js"
+  },
+  {
+    "revision": "b598b38c55ed1b0a7316",
+    "url": "/static/js/runtime-main.4bd32532.js"
+  },
+  {
+    "revision": "83553e15187b6987f894f2294bb30fa0",
+    "url": "/static/media/001-katana.83553e15.svg"
+  },
+  {
+    "revision": "e80134f8d4dc69abb5694071c112da1f",
+    "url": "/static/media/brocolli.e80134f8.png"
+  },
+  {
+    "revision": "c0894c3dcb92be42b7efef61250c379c",
+    "url": "/static/media/coconut.c0894c3d.jpg"
+  },
+  {
+    "revision": "a09b91ebfaaa39e65434a044d8df6cb2",
+    "url": "/static/media/eggs.a09b91eb.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "a0ccafc923290027cc86fb253bc81773",
+    "url": "/static/media/fruit.a0ccafc9.png"
+  },
+  {
+    "revision": "b5cfad486d4f08e165713006766c14b3",
+    "url": "/static/media/fruits.b5cfad48.jpg"
+  },
+  {
+    "revision": "a9cb2edd166708c73fab9817d5634569",
+    "url": "/static/media/grain.a9cb2edd.png"
+  },
+  {
+    "revision": "5af00e8b31fe3d8af89bc681d40ffdce",
+    "url": "/static/media/meat.5af00e8b.jpg"
+  },
+  {
+    "revision": "a21d4234bc9e3def875269ff9c2974a1",
+    "url": "/static/media/nuts.a21d4234.jpg"
+  },
+  {
+    "revision": "99fd7e316f4ded3df9daa9fa8679ace4",
+    "url": "/static/media/rice.99fd7e31.jpg"
+  },
+  {
+    "revision": "8074ed6f6b50771dccc25ca75dfd1093",
+    "url": "/static/media/vegetables.8074ed6f.jpg"
+  }
+]);
